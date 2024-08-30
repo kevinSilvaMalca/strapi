39e8e1ce0ef931bffe4e0a0661591c4e006361d6
root <root@vmi2038129.contaboserver.net>
2024-08-30 19:23:32 -0400

    collectionType

---
+++ src/api/flayer/controllers/flayer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * flayer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::flayer.flayer');
---
+++ src/api/flayer/routes/flayer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * flayer router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::flayer.flayer');
