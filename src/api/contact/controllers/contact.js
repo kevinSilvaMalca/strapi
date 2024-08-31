6d323f2e75c962638d96e78aebd7a0297db25eb6
ksilva_mvapp <ksilva@moveapps.cl>
2024-08-31 13:44:18 -0500

    Updated New Controller

--- src/api/contact/controllers/contact.js
+++ src/api/contact/controllers/contact.js
@@ -1,9 +1,40 @@
 'use strict';
 
-/**
- * contact service
- */
+const { createCoreController } = require('@strapi/strapi').factories;
 
-const { createCoreService } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::contact.contact', ({ strapi }) => ({
+  async create(ctx) {
+    // Llamar al método create predeterminado
+    const response = await super.create(ctx);
 
-module.exports = createCoreService('api::contact.contact');
+    // Extraer los datos del contacto recién creado
+    const contact = response.data;
+
+    const dateObj = new Date(contact.attributes.date);
+    const formattedDate = `${dateObj.getDate().toString().padStart(2, '0')}-${(dateObj.getMonth() + 1).toString().padStart(2, '0')}-${dateObj.getFullYear()} ${dateObj.getHours().toString().padStart(2, '0')}:${dateObj.getMinutes().toString().padStart(2, '0')}`;
+
+    // Configurar el correo electrónico
+    const emailOptions = {
+      to: contact.attributes.email,
+      from: 'thetriplethree@innvortex.com', // Cambia esto por tu dirección de correo
+      template_id: 'd-10696c49bef845fabeca7c1bd13db33f', // El template ID de SendGrid
+      dynamic_template_data: {
+        name: contact.attributes.name,
+        email: contact.attributes.email,
+        phone: contact.attributes.phone,
+        numberOfPeople: contact.attributes.numberOfPeople,
+        date: formattedDate,
+      }
+    };
+
+    // Enviar el correo utilizando el servicio de email de Strapi
+    try {
+      await strapi.plugin('email').service('email').send(emailOptions);
+      strapi.log.info('Correo enviado con éxito a', contact.attributes.email);
+    } catch (error) {
+      strapi.log.error('Error al enviar el correo:', error);
+    }
+
+    return response;
+  },
+}));
