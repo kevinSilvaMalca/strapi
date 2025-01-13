81f71b14ede9594f2e7cbeb7dcab1e7b92ac1e56
ksilva_mvapp <ksilva@moveapps.cl>
2025-01-13 00:02:47 -0500

    updated

--- src/api/url-permantly/controllers/url-permantly.js
+++ src/api/url-permantly/controllers/url-permantly.js
@@ -9,9 +9,12 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::url-permantly.url-permantly', ({ strapi }) => ({
   async findBySlug(ctx) {
     const { slug } = ctx.params;
+    console.log('slug', slug);
     const entity = await strapi.db.query('api::url-permantly.url-permantly').findOne({
       where: { slug },
     });
+
+    console.log('entity', entity);
 
     if (!entity) {
       return ctx.notFound('URL not found');
