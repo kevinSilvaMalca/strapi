bab48c7029ff4436c7ac57d8193f6224e63db38b
ksilva_mvapp <ksilva@moveapps.cl>
2025-01-14 00:16:36 -0500

    updateddata

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://admin.thetriplethree333.com', 
   app: {
     keys: env.array('APP_KEYS'),
   },
