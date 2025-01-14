380177157bb1e478bd74d9d20c0506acba1523a7
ksilva_mvapp <ksilva@moveapps.cl>
2025-01-14 00:00:22 -0500

    updated

---
+++ src/api/url-permantly/content-manager/url-permantly/content-manager.js
@@ -0,0 +1,16 @@
+module.exports = {
+    edit: {
+      fields: {
+        slug: {
+          editable: false,
+        },
+      },
+    },
+    create: {
+      fields: {
+        slug: {
+          editable: false,
+        },
+      },
+    },
+  };
